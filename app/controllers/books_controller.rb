e6482df759118cb55e0e00a6f36296fb2cefc073
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-02-02 05:58:33 +0000

    [Finish]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,62 @@
+class BooksController < ApplicationController
+before_action :ensure_correct_user, only: [:edit, :update]
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @books_new = Book.new
+  end
+
+  def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+    flash[:notice] = "You have created book successfully."
+   redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @users = User.all
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+       @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully"
+      redirect_to book_path(@book.id)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "you have destroyed book successfully."
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :profile_image, :introduction)
+  end
+def ensure_correct_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
+end
+end
