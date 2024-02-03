3b0505d34f3d5b79727ed95455ad1c64343bc630
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-02-03 12:18:16 +0000

    [bookers2]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,29 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  has_many :followers, through: :passive_relationships, source: :follower      
-  has_many :followings, through: :active_relationships, source: :followed       
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy      
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy      
+
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   has_many :favorites, dependent: :destroy
   has_many :books,dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
-  
+
 
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
+
   def following?(user)
     followings.include?(user)
   end
-end
-  
+
+
   def unfollow(user)
     active_relationships.find_by(followed_id: user.id).destroy
   end
-  
+
   def follow(user)
     active_relationships.create(followed_id: user.id)
   end
@@ -37,6 +38,7 @@ end
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ end
   resources :books do
   resources :book_comments
 end
+
+
+
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
