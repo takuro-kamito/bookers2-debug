13f2e03a0e0127034d4dd31fb12c2632a4ed5470
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-05-09 02:25:34 +0000

    DM機能修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_08_093735) do
+ActiveRecord::Schema.define(version: 2024_05_09_013233) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2024_05_08_093735) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
